1ebc80b541bf905806f5332fc356ad0cc4b29af6
james.franklin <james.franklin@protecht.com.au>
2022-03-16 00:15:39 +1100

    css crapola

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -5,17 +5,17 @@ export default function Topbar() {
     <div className="topbar">
       <div className="wrapper">
         <div className="button1">
-          <a href="#intro">About me</a>
+          <a href="#intro"><h1>James</h1></a>
         </div>
         <div className="button2">
-          <a href="#projects">My projects</a>
+          <a href="#projects"><h1>Projects</h1></a>
         </div>
         <div className="button3">
-          <a href="#contact">Contact</a>
+          <a href="#contact"><h1>Contact</h1></a>
         </div>
         
       </div>
-      topbar
+      
     </div>
   )
 }
