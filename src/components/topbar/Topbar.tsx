e0ad9e89368ee9988e6b28be6c09c71aefc2afc7
james.franklin <james.franklin@protecht.com.au>
2022-03-15 23:51:27 +1100

    beautifying

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -3,6 +3,18 @@ import "./topbar.scss"
 export default function Topbar() {
   return (
     <div className="topbar">
+      <div className="wrapper">
+        <div className="button1">
+          <a href="#intro">About me</a>
+        </div>
+        <div className="button2">
+          <a href="#projects">My projects</a>
+        </div>
+        <div className="button3">
+          <a href="#contact">Contact</a>
+        </div>
+        
+      </div>
       topbar
     </div>
   )
