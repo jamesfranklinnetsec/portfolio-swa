ef5e574278d454a77dbf41387b50e0e26f8bab3a
james.franklin <james.franklin@protecht.com.au>
2022-03-15 21:50:21 +1100

    include bits in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Contact from "./components/contact/Contact";
+import Projects from "./components/projects/Projects";
+
 
 function App() {
   return (
     <div className="app">
-      hello
+      <Topbar/>
+      <Contact/>
+      <Intro/>
+      <Projects/>
     </div>
   );
 }
