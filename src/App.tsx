1eda20dafe14e4f6d9ac7eb901698f5de10ece30
james.franklin <james.franklin@protecht.com.au>
2022-03-15 22:33:27 +1100

    i dunno

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
+/**
 import './App.css';
+*/
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Contact from "./components/contact/Contact";
 import Projects from "./components/projects/Projects";
-
+import "./app.scss";
 
 function App() {
   return (
     <div className="app">
       <Topbar/>
-      <Contact/>
-      <Intro/>
-      <Projects/>
+      <div className="sections">
+        <Intro/>
+        <Projects/>
+        <Contact/>
+
+      </div>
     </div>
   );
 }
