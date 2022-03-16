ae3f61c187cb394496a00014027cf5c4f41a6336
james.franklin <james.franklin@protecht.com.au>
2022-03-16 22:40:23 +1100

    fixed text colour by added class in href

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -5,17 +5,17 @@ export default function Topbar() {
     <div className="topbar">
       <div className="wrapper">
         <div className="left">
-        <a href="#intro"><h1>Logo</h1></a>
+        <a href="#intro" className="logo"><h1>Logo</h1></a>
 
         </div>
         <div className="button1">
-          <a href="#intro"><h1>James</h1></a>
+          <a href="#intro" className="logo"><h2>James</h2></a>
         </div>
         <div className="button2">
-          <a href="#projects"><h1>Projects</h1></a>
+          <a href="#projects" className="logo"><h2>Projects</h2></a>
         </div>
         <div className="button3">
-          <a href="#contact"><h1>Contact</h1></a>
+          <a href="#contact" className="logo"><h2>Contact</h2></a>
         </div>
         
       </div>
